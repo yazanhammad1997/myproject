720a7ee81dace55d60d7bade9f0371dc6d38a26f
Yazanhammad1997 <yazanhammad737@gmail.com>
2023-09-28 12:35:50 +0300

    complete lab

---
+++ java/as1/describe('index.js
@@ -0,0 +1,10 @@
+describe('index.js', function () {
+    // there's stuff in here, too
+});
+it('is set as Scuber', function () {
+    // tests are here
+});
+t('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+});
+let companyName = 'Scuber';
